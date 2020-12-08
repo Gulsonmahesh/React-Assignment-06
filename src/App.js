061610c6a3623394c13eb36e1caa24608e75989f
Gulson Mahesh <mahesh.madasamy@wipro.com>
2020-12-08 12:40:35 +0530

    Update Error Boundary

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React, { Component, Fragment } from 'react';
-import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import React, { Component, Fragment, lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Navigator from './product/naviagator';
 import About from './product/about';
-import Addproduct from './product/addproduct';
-import Allproduct from './product/allproduct';
+// import Addproduct from './product/addproduct';
+// import Allproduct from './product/allproduct';
+import ErrorWrapper from './ErrorWrapper';
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
+      <ErrorWrapper>
       <div className="container">
-        <BrowserRouter>
+        <Router>
         <div>
           <Navigator />
           <Switch>
             <Route path="/" component={About} exact/>
-            <Route path="/about/:product" component={About} />
-            <Route path="/addproduct" component={Addproduct} />
-            <Route path="/products" component={Allproduct} />
+            <Route path="/about/:product" component={lazy(() => import ('././product/about'))} />
+            <Route path="/addproduct" component={lazy(() => import './product/addproduct')} />
+            <Route path="/products" component={lazy (() => impoer ('./product/allproduct'))} />
           </Switch>
         </div>
-        </BrowserRouter>
+        </Router>
         </div>
-      </Fragment>
+      </ErrorWrapper>
     )
   }
 }
